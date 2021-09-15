7d641df1a3afe7c30e6943f982a79e5c68bd3739
rebecaemuniz <73186206+rebecaemuniz@users.noreply.github.com>
2021-09-14 21:40:37 -0400

    Add files via upload

---
+++ ConsoleApplicationWalkthrough/pch.h
@@ -0,0 +1,7 @@
+﻿// pch.h
+#pragma once
+#pragma comment(lib, "windowsapp")
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Web.Syndication.h>
+#include <iostream>
+#include <winrt/base.h>
